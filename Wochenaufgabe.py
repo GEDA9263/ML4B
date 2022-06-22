f5585f50774105d1209825b7f8348ea3051c03ea
JorgoVassiliadis <104491218+JorgoVassiliadis@users.noreply.github.com>
2022-06-22 16:10:21 +0200

    Update Wochenaufgabe.py

--- Wochenaufgabe.py
+++ Wochenaufgabe.py
@@ -131,13 +131,13 @@ st.subheader("15.06.22 Evaluation")
 
 with st.expander("See more"):
      st.write("""Für die Evaluation haben wir ein paar Beispiele, um zu veranschaulichen, wie viel besser das Programm mit zunehmenden Training wird.
+      """)
      st.image("10epochen")
      st.image("20epochen")
+    
+     st.write("""Zwischenzeitlich hatten wir noch mit einen paar Bugs zu kämpfen zum Beispiel wurden uns immer die maximale Anzahl an Wörter in der Caption ausgegeben, anstatt eine Sinnvolle Caption. So ist zum Beispiel dieser Wörtersalat entstanden:    
      """)
-     st.write("""Zwischenzeitlich hatten wir noch mit einen paar Bugs zu kämpfen zum Beispiel wurden uns immer die maximale Anzahl an Wörter in der Caption ausgegeben, anstatt eine Sinnvolle Caption. So ist zum Beispiel dieser Wörtersalat entstanden:
-     st.image("brokencaption.jpg")     
-     """)
-
+st.image("brokencaption.jpg")
 
 
 
