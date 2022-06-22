160f0cc9d82f56d19f6581bc2acfd178be08345d
JorgoVassiliadis <104491218+JorgoVassiliadis@users.noreply.github.com>
2022-06-22 15:08:47 +0200

    Update Wochenaufgabe.py

--- Wochenaufgabe.py
+++ Wochenaufgabe.py
@@ -7,6 +7,15 @@ import os.path
 
 st.title('TEST')
 
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+     st.write("""
+         The chart above shows some numbers I picked for you.
+         I rolled actual dice for these, so they're *guaranteed* to
+         be random.
+     """)
+
 st.title('Projektpräsentation')
 st.subheader('Teamvorstellung')
 
