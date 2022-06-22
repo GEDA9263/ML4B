13c7c0421e46e077f4ff8c2578c2e84beaa6acef
JorgoVassiliadis <104491218+JorgoVassiliadis@users.noreply.github.com>
2022-06-22 15:11:16 +0200

    Update Wochenaufgabe.py

--- Wochenaufgabe.py
+++ Wochenaufgabe.py
@@ -5,7 +5,15 @@ from st_aggrid.shared import GridUpdateMode
 import requests
 import os.path
 
-st.expander(label, expanded=False)
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+     st.write("""
+         The chart above shows some numbers I picked for you.
+         I rolled actual dice for these, so they're *guaranteed* to
+         be random.
+     """)
+     st.image("https://static.streamlit.io/examples/dice.jpg")
 
 st.title('Projektpräsentation')
 st.subheader('Teamvorstellung')
