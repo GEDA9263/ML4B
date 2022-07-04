2f91e041a42bfa7ef84404cd212bc396eba37a90
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 13:32:44 +0200

    Update 03_03.05.22.py

--- pages/03_03.05.22.py
+++ pages/03_03.05.22.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import pandas as pd
-from st_aggrid import AgGrid, GridOptionsBuilder
-from st_aggrid.shared import GridUpdateMode
-import requests
-import os.path
-
-
 
 st.headerheader("03.05.22 - Gruppenaufteilung")
 st.write(""" In der ersten Woche haben wir mit Streamlit eine kleine erste App gebaut und die Installation der erforderlichen Programme durchgeführt. Es gab kleinere Schwierigkeiten beim Einarbeiten, da viele neue Programme auf einmal benötigt wurden und wir uns zuerst einen Überblick verschaffen mussten, welches Programm welche Funktion erfüllt.
