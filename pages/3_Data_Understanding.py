77d987fc5e88e67dcef78dd0fbb5b3b117b54c3d
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 13:59:47 +0200

    Update 3_Data_Understanding.py

--- pages/3_Data_Understanding.py
+++ pages/3_Data_Understanding.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.header("04.05.22 ")
+st.header("04.05.22 - CRISP-DM Data understanding")
 st.write("""
         In der Zweiten Woche haben wir uns näher mit unserem Datensatz auseinandergesetzt und uns genauer überlegt, was wir mit unserem Projekt eigentlich machen wollen. Zunächst haben wir hierfür Ideen gesammelt und uns dann auf Emergency Vehicles geeinigt. Das Datenset ist zwar sehr groß, aber einfach zu filtern mit Hilfe eines Web Programms (hier einfügen), zudem liegt pro Bild bereits eine Bildunterschrift in verschiedenen Sprachen vor. Als erstes haben wir uns auf die Sprache Englisch festgelegt.
         """)
