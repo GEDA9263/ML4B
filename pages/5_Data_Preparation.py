0aabdcbc0dbc5e3a448cc7449e8ab5ee975b42f6
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 14:06:32 +0200

    Update 5_Data_Preparation.py

--- pages/5_Data_Preparation.py
+++ pages/5_Data_Preparation.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import pandas as pd
 
 st.header("18.05.22 - Data Preparation")
 st.write("""
         Zunächst haben wir den Datensatz eingeschränkt. Mit den Suchbegriffen „vehicle“, „emergency vehicle“ und „traffic“ etc. Schnell haben wir gemerkt, dass der Datensatz für unser Thema zu klein ist, um einen Algorithmus sinnvoll zu trainieren. Außerdem hatten wir Schwierigkeiten sinnvolle Bilder zu finden, da beispielsweise viele Spielzeugautos mit in unserem Datensatz waren.
         """)
+
+df = pd.read_json('trafficSubset.json')
+selection = aggrid_interactive_table(df)
+
+if len(selection["selected_rows"]) > 0:
+    st.write("Hier ist das ausgewählte Bild! ")
+    url = selection["selected_rows"][0]["url"]
+    id = selection["selected_rows"][0]["id"]
+    filename = str(id) + '.jpg'
+    bool = os.path.exists('pictures/' + filename)
+    if not bool:
+        r = requests.get(url, allow_redirects=True)
+        open("pictures/" + str(id) + '.jpg', "wb").write(r.content)
+    st.image('pictures/' + str(id) + '.jpg')
+    st.write(selection["selected_rows"][0]["caption"])
