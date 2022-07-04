834f4cc4317151a30a0f11c90f36c4397c6e67f4
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 14:57:26 +0200

    Update 11_💾_Trainingsdaten.py

--- pages/11_💾_Trainingsdaten.py
+++ pages/11_💾_Trainingsdaten.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
 st.title("Bilder")
-st.sidebar.selectbox('Bitte wählen sie die Daten aus', ('Epoche 10', 'Epoche 20'))
+option = st.sidebar.selectbox('Bitte wählen sie die Daten aus', ('Epoche 10', 'Epoche 20'))
 
+if(option == 'Epoche 10'):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("Trainingsbilder/Pferde/Pferde_10_Epochen.jpg")
+    with col2:
+        st.image("Trainingsbilder/Elefanten/10 Epochen Elefant.png")
+        
+        
 with st.expander("Epoche 10"):
     col1, col2 = st.columns(2)
 with col1:    
