2f3a22f62cc54502fb384bc82029a82947d19446
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 15:09:36 +0200

    Update 11_💾_Trainingsdaten.py

--- pages/11_💾_Trainingsdaten.py
+++ pages/11_💾_Trainingsdaten.py
@@ -1,60 +1,29 @@
 import streamlit as st
 
 st.title("Bilder")
-option = st.sidebar.selectbox('Bitte wählen sie die Daten aus', ('Epoche 10', 'Epoche 20'))
+option = st.sidebar.selectbox('Bitte wählen sie die Daten aus', ('Epoche 10', 'Epoche 20', 'Epoche 30', 'Epoche 40', 'Epoche 50', 'Epoche 60', 'Epoche 70', 'Verlustgrafik'))
+breite = 720
 
 if(option == 'Epoche 10'):
-    st.image("Trainingsbilder/Pferde/Pferde_10_Epochen.jpg", width=720)
-    st.image("Trainingsbilder/Elefanten/10 Epochen Elefant.png", width=720)
-        
-        
-with st.expander("Epoche 10"):
-    col1, col2 = st.columns(2)
-with col1:    
-    st.image("Trainingsbilder/Pferde/Pferde_10_Epochen.jpg")
-with col2:
-    st.image("Trainingsbilder/Elefanten/10 Epochen Elefant.png")
-         
-          
-with st.expander("Epoche 20"):
-    col1, col2 = st.columns(2)
-with col1:    
-    st.image("Trainingsbilder/Pferde/Pferde_20_Epochen.jpg")
-with col2:
-    st.image("Trainingsbilder/Elefanten/30 Epochen Elefant.png")
-         
-             
-with st.expander("Epoche 30"):
-    col1, col2 = st.columns(2)
-with col1:    
-    st.image("Trainingsbilder/Pferde/Pferde_30_Epochen.jpg")
-with col2:
-    st.image("Trainingsbilder/Elefanten/30 Epochen Elefant.png")
-
-
-with st.expander("Epoche 40"):
-    col1, col2 = st.columns(2)
-with col1:    
-    st.image("Trainingsbilder/Pferde/Pferde_40_Epochen.jpg")
-with col2:
-    st.image("Trainingsbilder/Elefanten/40 Epochen Elefant.png")
-
-
-with st.expander("Epoche 50"):
-    col1, col2 = st.columns(2)
-with col1:    
-    st.image("Trainingsbilder/Pferde/Pferde_50_Epochen.jpg")
-with col2:
-    st.image("Trainingsbilder/Elefanten/50 Epochen Elefant.png")
-
-with st.expander("Epoche 60 & 70"):
-    col1, col2 = st.columns(2)
-with col1:    
-    st.image("Trainingsbilder/Pferde/Pferde_60_Epochen.jpg")
-with col2:
-    st.image("Trainingsbilder/Elefanten/60 Epochen Elefant.png")
-    st.image("Trainingsbilder/Elefanten/70 Epochen Elefant.png")
-         
-         
-with st.expander("Epochen Loss Plot"):
-          st.image("Trainingsbilder/Pferde/Pferde_Loss_Plot.jpg")
+    st.image("Trainingsbilder/Pferde/Pferde_10_Epochen.jpg", width=breite)
+    st.image("Trainingsbilder/Elefanten/10 Epochen Elefant.png", width=breite)        
+elif(option == 'Epoche 20'):
+    st.image("Trainingsbilder/Pferde/Pferde_20_Epochen.jpg", width=breite)
+    st.image("Trainingsbilder/Elefanten/20 Epochen Elefant.png",  width=breite)
+elif(option == 'Epoche 30'):
+    st.image("Trainingsbilder/Pferde/Pferde_30_Epochen.jpg", width=breite)
+    st.image("Trainingsbilder/Elefanten/30 Epochen Elefant.png",  width=breite)
+elif(option == 'Epoche 40'):
+    st.image("Trainingsbilder/Pferde/Pferde_40_Epochen.jpg", width=breite)
+    st.image("Trainingsbilder/Elefanten/40 Epochen Elefant.png",  width=breite0)
+elif(option == 'Epoche 50'):
+    st.image("Trainingsbilder/Pferde/Pferde_50_Epochen.jpg", width=breite)
+    st.image("Trainingsbilder/Elefanten/50 Epochen Elefant.png",  width=breite)
+elif(option == 'Epoche 60'):
+    st.image("Trainingsbilder/Pferde/Pferde_60_Epochen.jpg", width=breite)
+    st.image("Trainingsbilder/Elefanten/60 Epochen Elefant.png",  width=breite)
+elif(option == 'Epoche 70'):
+    st.image("Trainingsbilder/Elefanten/70 Epochen Elefant.png",  width=breite)
+elif(option == 'Verlustgrafik'):
+     st.image("Trainingsbilder/Pferde/Pferde_Loss_Plot.jpg", width=breite)
+     st.image(("Trainingsbilder/Elefanten/Verlustdiagramm 70 Epochen.png", width=breite)
