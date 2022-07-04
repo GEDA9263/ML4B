b0280fc1a21794a4e780ddf394115808c3c8dd28
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 14:48:31 +0200

    Update 9_👍_Evaluation.py

--- pages/9_👍_Evaluation.py
+++ pages/9_👍_Evaluation.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.header("15.06.22 ")
+st.header("15.06.22 - Evaluation")
 st.write("""Für die Evaluation haben wir ein paar Beispiele, um zu veranschaulichen, wie viel besser das Programm mit zunehmenden Training wird.""")
 st.write("""Zwischenzeitlich hatten wir noch mit ein paar Bugs zu kämpfen, zum Beispiel wurden uns immer die maximale Anzahl an Wörter in der Caption ausgegeben, anstatt eine Sinnvolle Caption. So ist zum Beispiel dieser Wörtersalat entstanden:    
          """)
