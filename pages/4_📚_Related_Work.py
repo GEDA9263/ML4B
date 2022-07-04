3a2ca40bce29ad1ed268923229a3a6e12fefd23c
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 15:22:35 +0200

    Update 4_📚_Related_Work.py

--- pages/4_📚_Related_Work.py
+++ pages/4_📚_Related_Work.py
@@ -9,5 +9,9 @@ st.markdown(
          Gefundene Ressourcen, welche Code beinhalteten, waren zum Beispiel solche: 
          - https://github.com/BakingBrains/Emergency_Vehicle_Detection_-classification-/blob/main/Emergency%20Vehicle%20Identification.ipynb
          - https://github.com/DhilipSanjay/ANN-with-Momentum
+         Hier noch eine Auswahlan anderen Artikeln, welche wir besonders interessant fanden:
+         - https://proceedings.neurips.cc/paper/2019/file/680390c55bbd9ce416d1d69a9ab4760d-Paper.pdf
+         - https://link.springer.com/content/pdf/10.1007/978-3-030-89701-7_2.pdf (Frei downloadbar mit einem FAU Zugang)
+         - https://www.researchgate.net/publication/347959945_The_Traffic_Scene_Understanding_and_Prediction_Based_on_Image_Captioning/fulltext/5feecaaca6fdccdcb81eb998/The-Traffic-Scene-Understanding-and-Prediction-Based-on-Image-Captioning.pdf?origin=publication_detail
          """
 )
