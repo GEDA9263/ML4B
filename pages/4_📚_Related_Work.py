3e60c933fa6b62b675acc219ec59e1eaa1076fd8
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 15:17:26 +0200

    Update 4_📚_Related_Work.py

--- pages/4_📚_Related_Work.py
+++ pages/4_📚_Related_Work.py
@@ -4,3 +4,12 @@ st.header("11.05.22 - Related Work")
 st.write("""
          In dieser Woche haben wir begonnen zu recherchieren. Zunächst hat jeder für sich mit verschiedenen Suchmaschinen recherchiert. Wir haben uns generell  über Machine Learning im Image Captioning Bereich eingelesen, um gezielt nach Emergency Vehicle Erkennungen zu suchen. Tatsächlich waren auch einige relevante Paper dabei, teilweise sogar mit Code.
          """)
+st.markdown(
+         """"
+         Gefundene Ressourcen, welche Code beinhalteten, waren zum Beispiel solche: 
+         - https://github.com/BakingBrains/Emergency_Vehicle_Detection_-classification-/blob/main/Emergency%20Vehicle%20Identification.ipynb
+         - https://github.com/DhilipSanjay/ANN-with-Momentum
+
+
+         """"
+)
