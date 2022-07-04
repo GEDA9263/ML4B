7d46a8d048541bb87518ac12a4d974f9b04f8c10
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 14:02:47 +0200

    Update 4_Related_Work.py

--- pages/4_Related_Work.py
+++ pages/4_Related_Work.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.header("11.05.22 ")
+st.header("11.05.22 - Related Work")
 st.write("""
          In dieser Woche haben wir begonnen zu recherchieren. Zunächst hat jeder für sich mit verschiedenen Suchmaschinen recherchiert. Wir haben uns generell  über Machine Learning im Image Captioning Bereich eingelesen, um gezielt nach Emergency Vehicle Erkennungen zu suchen. Tatsächlich waren auch einige relevante Paper dabei, teilweise sogar mit Code.
          """)
