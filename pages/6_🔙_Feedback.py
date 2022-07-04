05a080124770a55b1ec9f4f57b31ae457acc4544
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 17:08:51 +0200

    Update 6_🔙_Feedback.py

--- pages/6_🔙_Feedback.py
+++ pages/6_🔙_Feedback.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
-st.header("25.05.22 - Feedback")
+st.set_page_config(
+    page_title="Feedback",
+    page_icon="🔙",
+)
+
+st.header("🔙 - 25.05.22 - Feedback")
 st.write("""Nachdem wir unseren aktuellen Stand der Recherche präsentiert haben, haben wir als Feedback bekommen uns nicht ganz so zu beschränken und noch einmal einen Schritt zurück zu gehen. Daraufhin haben wir erneut begonnen zu Recherchieren und sind auf ein Git repository gestoßen, welches vielversprechend aussah. Somit haben wir unser Thema eher in die Richtung des General Purpose Image Captioning Algorithmus verändert. Der neue Datensatz den wir mit diesem Model gefunden haben, hat sogar 4 Captions pro Bild. Zudem hätten wir den alten Datensatz aufwendig an das benötigte Format anpassen müssen.
     """)
