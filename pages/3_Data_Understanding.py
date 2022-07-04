4385b4d81ccd89ead2f8b7c85ef7bbb74c422fab
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 13:58:44 +0200

    Update 3_Data_Understanding.py

--- pages/3_Data_Understanding.py
+++ pages/3_Data_Understanding.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.subheader("04.05.22 ")
+st.header("04.05.22 ")
 st.write("""
         In der Zweiten Woche haben wir uns näher mit unserem Datensatz auseinandergesetzt und uns genauer überlegt, was wir mit unserem Projekt eigentlich machen wollen. Zunächst haben wir hierfür Ideen gesammelt und uns dann auf Emergency Vehicles geeinigt. Das Datenset ist zwar sehr groß, aber einfach zu filtern mit Hilfe eines Web Programms (hier einfügen), zudem liegt pro Bild bereits eine Bildunterschrift in verschiedenen Sprachen vor. Als erstes haben wir uns auf die Sprache Englisch festgelegt.
         """)
