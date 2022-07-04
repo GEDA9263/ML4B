0d317fed0306bf0f4d36792c92fafca56c4e3b34
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 17:13:51 +0200

    Update 10_🎓_Deployment.py

--- pages/10_🎓_Deployment.py
+++ pages/10_🎓_Deployment.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Deployment",
+    page_icon="🎓",
+)
+
 st.header("22.06.22 - Deployment")
 st.write("""Die letzten Tage vor der Abgabe haben wir nun gebraucht, um die Streamlit app aufzuhübschen.
      """)
