7c33d2c1083efd59bb229267e071c71acc688813
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 17:01:29 +0200

    Update 2_🚶_Erste_Schritte.py

--- pages/2_🚶_Erste_Schritte.py
+++ pages/2_🚶_Erste_Schritte.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Projektpräsentation",
-    page_icon="📽️",
+    page_title="Erste Schritte",
+    page_icon="🚶",
 )
 
-st.header("03.05.22 - Gruppenaufteilung")
+st.header("🚶 - 03.05.22 - Gruppenaufteilung")
 st.write(""" In der ersten Woche haben wir mit Streamlit eine kleine erste App gebaut und die Installation der erforderlichen Programme durchgeführt. Es gab kleinere Schwierigkeiten beim Einarbeiten, da viele neue Programme auf einmal benötigt wurden und wir uns zuerst einen Überblick verschaffen mussten, welches Programm welche Funktion erfüllt.
      """)
