35f6d0a3b4479a1ec2e524e461fcc6ef9b3380dd
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 14:04:05 +0200

    Create 5_Data_Preparation.py

---
+++ pages/5_Data_Preparation.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header("18.05.22 - Data Preparation")
+st.write("""
+        Zunächst haben wir den Datensatz eingeschränkt. Mit den Suchbegriffen „vehicle“, „emergency vehicle“ und „traffic“ etc. Schnell haben wir gemerkt, dass der Datensatz für unser Thema zu klein ist, um einen Algorithmus sinnvoll zu trainieren. Außerdem hatten wir Schwierigkeiten sinnvolle Bilder zu finden, da beispielsweise viele Spielzeugautos mit in unserem Datensatz waren.
+        """)
