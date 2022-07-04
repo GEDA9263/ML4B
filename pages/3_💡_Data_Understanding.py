b1eaacb86bced621a9d487a7bff31a5031256293
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 17:03:13 +0200

    Update 3_💡_Data_Understanding.py

--- pages/3_💡_Data_Understanding.py
+++ pages/3_💡_Data_Understanding.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.header("04.05.22 - CRISP-DM Data understanding")
+st.set_page_config(
+    page_title="Data understanding",
+    page_icon="💡",
+)
+
+st.header("💡 - 04.05.22 - CRISP-DM Data understanding")
 st.write("""
-        In der Zweiten Woche haben wir uns näher mit unserem Datensatz auseinandergesetzt und uns genauer überlegt, was wir mit unserem Projekt eigentlich machen wollen. Zunächst haben wir hierfür Ideen gesammelt und uns dann auf Emergency Vehicles geeinigt. Das Datenset ist zwar sehr groß, aber einfach zu filtern mit Hilfe eines Web Programms (https://rom1504.github.io/clip-retrieval/?back=https%3A%2F%2Fknn5.laion.ai&index=laion5B&useMclip=false), zudem liegt pro Bild bereits eine Bildunterschrift in verschiedenen Sprachen vor. Als erstes haben wir uns auf die Sprache Englisch festgelegt.
+        In der Zweiten Woche haben wir uns näher mit unserem Datensatz auseinandergesetzt und uns genauer überlegt, was wir mit unserem Projekt eigentlich machen wollen. Zunächst haben wir hierfür Ideen gesammelt und uns dann auf Emergency Vehicles geeinigt. Das Datenset ist zwar sehr groß, aber einfach zu filtern ✂️ mit Hilfe eines Web Programms (https://rom1504.github.io/clip-retrieval/?back=https%3A%2F%2Fknn5.laion.ai&index=laion5B&useMclip=false), zudem liegt pro Bild bereits eine Bildunterschrift in verschiedenen Sprachen vor. Als erstes haben wir uns auf die Sprache Englisch festgelegt.
         """)
