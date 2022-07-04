a4526fb0eb6ffbc39c21b508af3e3a8cb053e079
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 13:41:05 +0200

    Update 02_Erste Schritte.py

--- pages/02_Erste Schritte.py
+++ pages/02_Erste Schritte.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.headerheader("03.05.22 - Gruppenaufteilung")
+st.header("03.05.22 - Gruppenaufteilung")
 st.write(""" In der ersten Woche haben wir mit Streamlit eine kleine erste App gebaut und die Installation der erforderlichen Programme durchgeführt. Es gab kleinere Schwierigkeiten beim Einarbeiten, da viele neue Programme auf einmal benötigt wurden und wir uns zuerst einen Überblick verschaffen mussten, welches Programm welche Funktion erfüllt.
      """)
