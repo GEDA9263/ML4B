9ffcec2fb7ad6fabdb7c0ae50765971a8a5b67b0
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 13:46:51 +0200

    Update Wochenaufgabe.py

--- Wochenaufgabe.py
+++ Wochenaufgabe.py
@@ -5,7 +5,7 @@ from st_aggrid.shared import GridUpdateMode
 import requests
 import os.path
 
-st.sidebar
+st.sidebar.success("Wählen Sie bitte eine Seite aus")
 st.title('Projektpräsentation')
 st.subheader('Teamvorstellung')
 
