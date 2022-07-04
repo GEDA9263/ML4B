3763492d84b5fe38e41da1fe2cf9dd4cef9fba17
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 18:18:49 +0200

    Update 7_🖥️_Modeling.py

--- pages/7_🖥️_Modeling.py
+++ pages/7_🖥️_Modeling.py
@@ -6,5 +6,5 @@ st.set_page_config(
 )
 
 st.header("🖥️ - 01.06.22 - Modeling")
-st.write("""Diese Woche haben wir dafür genutzt, unseren Algorithmus zum Laufen zu bringen. Zunächst hatten wir versucht mit dem alten Datenset zu arbeiten. Was aufgrund des Datenformats nicht funktioniert hat. Zudem nutzen wir drei Modelle: Inception v3 tensorflow Machine Learning und Text vectorasation Sowie ein Imagecaptioning Model. Hier ist viel Zeit und Arbeit reingeflossen, um diese drei Modelle zusammen zum Laufen zu bringen.
+st.write("""Diese Woche haben wir dafür genutzt, unseren Algorithmus zum Laufen zu bringen. Zunächst hatten wir versucht mit dem alten Datenset zu arbeiten. Was aufgrund des Datenformats nicht funktioniert hat. Zudem nutzen wir drei Modelle: Inception v3 tensorflow Machine Learning und TextVectorization, sowie ein Imagecaptioning Model. Hier ist viel Zeit und Arbeit reingeflossen, um diese drei Modelle zusammen zum Laufen zu bringen.
     """)
