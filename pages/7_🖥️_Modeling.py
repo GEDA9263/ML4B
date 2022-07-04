5f8235255923d96bab9fac88137f49effc5b2b62
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 14:49:03 +0200

    Update 7_🖥️_Modeling.py

--- pages/7_🖥️_Modeling.py
+++ pages/7_🖥️_Modeling.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.header("01.06.22 ")
+st.header("01.06.22 - Modeling")
 st.write("""Diese Woche haben wir dafür genutzt, unseren Algorithmus zum Laufen zu bringen. Zunächst hatten wir versucht mit dem alten Datenset zu arbeiten. Was aufgrund des Datenformats nicht funktioniert hat. Zudem nutzen wir drei Modelle: Inception v3 tensorflow Machine Learning und Text vectorasation Sowie ein Imagecaptioning Model. Hier ist viel Zeit und Arbeit reingeflossen, um diese drei Modelle zusammen zum Laufen zu bringen.
     """)
