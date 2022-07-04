7b46e92715c2cd25570dde3c213b9b64bed0bda3
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 18:10:26 +0200

    Update 6_🔙_Feedback.py

--- pages/6_🔙_Feedback.py
+++ pages/6_🔙_Feedback.py
@@ -10,3 +10,26 @@ st.header("🔙 - 25.05.22 - Feedback")
 st.write("""Nachdem wir unseren aktuellen Stand der Recherche präsentiert haben, haben wir als Feedback bekommen uns nicht ganz so zu beschränken und noch einmal einen Schritt zurück zu gehen. Daraufhin haben wir erneut begonnen zu Recherchieren und sind auf ein Git repository gestoßen, welches vielversprechend aussah. Somit haben wir unser Thema eher in die Richtung des General Purpose Image Captioning Algorithmus verändert. Der neue Datensatz den wir mit diesem Model gefunden haben, hat sogar 5 Captions pro Bild. Zudem hätten wir den alten Datensatz aufwendig an das benötigte Format anpassen müssen.
     """)
 
+images = ['https://farm4.staticflickr.com/3210/3016304198_977b66924f_z.jpg',
+          'https://farm3.staticflickr.com/2009/2456971230_5db8094526_z.jpg',
+          'https://farm4.staticflickr.com/3060/2971251148_d241e54ae2_z.jpg',
+          'https://farm3.staticflickr.com/2191/2187671374_8195774f3b_z.jpg',
+          'https://farm9.staticflickr.com/8522/8581450876_b7bca30450_z.jpg',
+          'https://farm6.staticflickr.com/5490/9187151684_5f54e2fa26_z.jpg',
+          'https://farm3.staticflickr.com/2224/2483765539_4babc436c5_z.jpg',
+          'https://farm4.staticflickr.com/3396/3447807165_e5052e9d4d_z.jpg',
+          'https://farm6.staticflickr.com/5522/9294690250_f1d3924b7f_z.jpg'
+         ]
+
+captions = ['a man braiding the mane of a horse',
+            'a group of vehicles passing through an intersection',
+            'a seaplane lands in a bay where a cruise ship is docked',
+            'a group of snowboarders posing for a photograph',
+            'a meal on a paper plate set on a wood table.',
+            'the white bus is going down the quiet city street.',
+            'an old fire hydrant with some graffiti on it',
+            'a man in a field who is flying a kite.',
+            'a baseball player on the field warming up with his bat.'
+            ]
+
+st.image(images, captions)
