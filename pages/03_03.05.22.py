c3fc2f568c4fdc7844b974798a5f898ea89a99dc
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-04 13:32:25 +0200

    Update 03_03.05.22.py

--- pages/03_03.05.22.py
+++ pages/03_03.05.22.py
@@ -5,8 +5,8 @@ from st_aggrid.shared import GridUpdateMode
 import requests
 import os.path
 
-st.title("Chronologischer Ablauf")
 
-st.subheader("03.05.22 - Gruppenaufteilung")
+
+st.headerheader("03.05.22 - Gruppenaufteilung")
 st.write(""" In der ersten Woche haben wir mit Streamlit eine kleine erste App gebaut und die Installation der erforderlichen Programme durchgeführt. Es gab kleinere Schwierigkeiten beim Einarbeiten, da viele neue Programme auf einmal benötigt wurden und wir uns zuerst einen Überblick verschaffen mussten, welches Programm welche Funktion erfüllt.
      """)
