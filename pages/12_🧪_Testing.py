3c4e1ddcd976148a28bbee1e7d447d92c1adc49e
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-05 15:19:34 +0200

    Update 12_🧪_Testing.py

--- pages/12_🧪_Testing.py
+++ pages/12_🧪_Testing.py
@@ -12,7 +12,7 @@ from PIL import Image
 import shutil
 
 def load_image(image_path):
-    img = tf.io.read_file(image_path)
+    img = tf.io.read_file(file)
     img = tf.io.decode_jpeg(img, channels=3)
     img = tf.keras.layers.Resizing(299, 299)(img)
     img = tf.keras.applications.inception_v3.preprocess_input(img)
