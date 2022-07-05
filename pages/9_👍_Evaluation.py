fc7ebe978d60bf72cdd294117cfd0aee24ad488a
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-05 16:29:23 +0200

    Update 9_👍_Evaluation.py

--- pages/9_👍_Evaluation.py
+++ pages/9_👍_Evaluation.py
@@ -10,3 +10,6 @@ st.write("""Für die Evaluation haben wir ein paar Beispiele, um zu veranschauli
 st.write("""Zwischenzeitlich hatten wir noch mit ein paar Bugs zu kämpfen, zum Beispiel wurden uns immer die maximale Anzahl an Wörter in der Caption ausgegeben, anstatt eine Sinnvolle Caption. So ist zum Beispiel dieser Wörtersalat entstanden:    
          """)
 st.image("Trainingsbilder/brokencaption.jpg")
+
+st.write("Ab und an war jedoch auch eine ziemlich lustige Vorhersage dabei:)
+st.image("Trainingsbilder/brown_cow.jpg")
