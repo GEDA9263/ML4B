cbe6d23a7bc1fe0d85b1bf21ace8d263978be48e
JorgoVassiliadis <104491218+JorgoVassiliadis@users.noreply.github.com>
2022-07-05 13:46:08 +0200

    Update 4_📚_Related_Work.py

--- pages/4_📚_Related_Work.py
+++ pages/4_📚_Related_Work.py
@@ -7,14 +7,14 @@ st.set_page_config(
 
 st.header("📚 - 11.05.22 - Related Work")
 st.write("""
-         In dieser Woche haben wir begonnen zu recherchieren. Zunächst hat jeder für sich mit verschiedenen Suchmaschinen recherchiert. Wir haben uns generell  über Machine Learning im Image Captioning Bereich eingelesen, um gezielt nach Emergency Vehicle Erkennungen zu suchen. Tatsächlich waren auch einige relevante Paper dabei, teilweise sogar mit Code.
+         In dieser Woche haben wir begonnen zu recherchieren. Zunächst hat jeder für sich mit verschiedenen Suchmaschinen recherchiert. Wir haben uns generell über Machine Learning im Image Captioning Bereich eingelesen, um gezielt nach Emergency Vehicle Erkennungen zu suchen. Tatsächlich waren auch einige relevante Paper dabei, teilweise sogar mit Code.
          """)
 st.markdown(
          """
-         Gefundene Ressourcen, welche Code beinhalteten, waren zum Beispiel solche: 
+         Ressourcen mit Code: 
          - https://github.com/BakingBrains/Emergency_Vehicle_Detection_-classification-/blob/main/Emergency%20Vehicle%20Identification.ipynb
          - https://github.com/DhilipSanjay/ANN-with-Momentum
-         Hier noch eine Auswahlan anderen Artikeln, welche wir besonders interessant fanden:
+         Auswahl an interessanter Artikel:
          - https://proceedings.neurips.cc/paper/2019/file/680390c55bbd9ce416d1d69a9ab4760d-Paper.pdf
          - https://link.springer.com/content/pdf/10.1007/978-3-030-89701-7_2.pdf (Frei downloadbar mittels FAU Zugang)
          - https://www.researchgate.net/publication/347959945_The_Traffic_Scene_Understanding_and_Prediction_Based_on_Image_Captioning/fulltext/5feecaaca6fdccdcb81eb998/The-Traffic-Scene-Understanding-and-Prediction-Based-on-Image-Captioning.pdf?origin=publication_detail
