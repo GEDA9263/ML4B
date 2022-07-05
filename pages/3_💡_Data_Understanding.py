e161f444b642a72464319cbf8e6f7e11b1bebcae
JorgoVassiliadis <104491218+JorgoVassiliadis@users.noreply.github.com>
2022-07-05 13:38:25 +0200

    Update 3_💡_Data_Understanding.py

--- pages/3_💡_Data_Understanding.py
+++ pages/3_💡_Data_Understanding.py
@@ -6,6 +6,18 @@ st.set_page_config(
 )
 
 st.header("💡 - 04.05.22 - CRISP-DM Data Understanding")
-st.write("""
-        In der Zweiten Woche haben wir uns näher mit unserem Datensatz auseinandergesetzt und uns genauer überlegt, was wir mit unserem Projekt eigentlich machen wollen. Zunächst haben wir hierfür Ideen gesammelt und uns dann auf Emergency Vehicles geeinigt. Das Datenset ist zwar sehr groß, aber einfach zu filtern ✂️ mit Hilfe eines Web Programms (https://rom1504.github.io/clip-retrieval/?back=https%3A%2F%2Fknn5.laion.ai&index=laion5B&useMclip=false), zudem liegt pro Bild bereits eine Bildunterschrift in verschiedenen Sprachen vor. Als erstes haben wir uns auf die Sprache Englisch festgelegt.
-        """)
+
+st.markdown(
+      """
+      In der zweiten Woche haben wir uns näher mit unserem Datensatz auseinandergesetzt und uns genauer überlegt, was wir mit unserem Projekt eigentlich machen möchten.
+      Zunächst haben wir hierfür Ideen gesammelt. Unsere ersten Ideen beinhalteten:
+      - Pilze
+      - Sitzgelegenheiten
+      - Zootiere
+      - Autos, Traktoren und Baufahrzeuge
+    
+      Letztendlich haben wir uns auf Emergency Vehicles geeinigt.
+      <br>      
+      Das LAION5B Datenset ist sehr groß, aber mit Hilfe eines Web Programms kann man Teile davon einfach filtern ✂️ (https://rom1504.github.io/clip-retrieval/?back=https%3A%2F%2Fknn5.laion.ai&index=laion5B&useMclip=false) Zudem liegt pro Bild bereits eine Bildunterschrift in verschiedenen Sprachen vor.
+      """
+)
