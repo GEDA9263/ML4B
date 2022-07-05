2841e7ec902cb5efebde6fb932e3fb8ebcccdde5
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-05 16:34:51 +0200

    Update 11_💾_Trainingsdaten.py

--- pages/11_💾_Trainingsdaten.py
+++ pages/11_💾_Trainingsdaten.py
@@ -10,9 +10,9 @@ option = st.sidebar.selectbox('Bitte wählen sie die Epoche aus zu der Sie die D
 breite = 720
 
 if(option == 'Rohbilder'):
-   st.image("Trainingsbilder/Pferde/Pferde.jpg", caption= 'two horses walking down a path beside a fence', width=breite)
-   st.image("Trainingsbilder/Elefanten/Elefanten.jpg", caption='a baby elephant walking next to a large elephant', width=breite)       
-   st.image("Trainingsbilder/Human/Humans.jpg", caption='a lot of people around and under a tent in a city', width=breite)        
+    st.image("Trainingsbilder/Pferde/Pferde.jpg", caption= 'two horses walking down a path beside a fence', width=breite)
+    st.image("Trainingsbilder/Elefanten/Elefanten.jpg", caption='a baby elephant walking next to a large elephant', width=breite)       
+    st.image("Trainingsbilder/Human/Humans.jpg", caption='a lot of people around and under a tent in a city', width=breite)        
 elif(option == 'Epoche 10'):
     st.image("Trainingsbilder/Pferde/Pferde_10_Epochen.jpg", width=breite)
     st.image("Trainingsbilder/Elefanten/10 Epochen Elefant.png", width=breite)
@@ -20,20 +20,27 @@ elif(option == 'Epoche 10'):
 elif(option == 'Epoche 20'):
     st.image("Trainingsbilder/Pferde/Pferde_20_Epochen.jpg", width=breite)
     st.image("Trainingsbilder/Elefanten/20 Epochen Elefant.png",  width=breite)
+    st.image("Trainingsbilder/Human/Human 20 Epochen.jpg", width=breite)        
 elif(option == 'Epoche 30'):
     st.image("Trainingsbilder/Pferde/Pferde_30_Epochen.jpg", width=breite)
     st.image("Trainingsbilder/Elefanten/30 Epochen Elefant.png",  width=breite)
+    st.image("Trainingsbilder/Human/Human 30 Epochen.jpg", width=breite)        
 elif(option == 'Epoche 40'):
     st.image("Trainingsbilder/Pferde/Pferde_40_Epochen.jpg", width=breite)
     st.image("Trainingsbilder/Elefanten/40 Epochen Elefant.png",  width=breite)
+    st.image("Trainingsbilder/Human/Human 40 Epochen.jpg", width=breite)        
 elif(option == 'Epoche 50'):
     st.image("Trainingsbilder/Pferde/Pferde_50_Epochen.jpg", width=breite)
     st.image("Trainingsbilder/Elefanten/50 Epochen Elefant.png",  width=breite)
+    st.image("Trainingsbilder/Human/Human 50 Epochen.jpg", width=breite)        
 elif(option == 'Epoche 60'):
     st.image("Trainingsbilder/Pferde/Pferde_60_Epochen.jpg", width=breite)
     st.image("Trainingsbilder/Elefanten/60 Epochen Elefant.png",  width=breite)
+    st.image("Trainingsbilder/Human/Human 60 Epochen.jpg", width=breite)        
 elif(option == 'Epoche 70'):
     st.image("Trainingsbilder/Elefanten/70 Epochen Elefant.png",  width=breite)
+    st.image("Trainingsbilder/Human/Human 70 Epochen.jpg", width=breite)        
 elif(option == 'Verlustgrafik'):
      st.image("Trainingsbilder/Pferde/Pferde_Loss_Plot.jpg", caption= 'Verlustgrafik der Pferde', width=int((breite/2)))
      st.image("Trainingsbilder/Elefanten/Verlustdiagramm 70 Epochen.png", caption = 'Verlustgrafik der Elefanten', width=int((breite/2)))
+     st.image("Trainingsbilder/Humann/Verlust Menschen.jpg", caption = 'Verlustgrafik der Ansammlung', width=int((breite/2)))
