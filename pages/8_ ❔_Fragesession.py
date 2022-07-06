1b622a63eaf38c57879616d4e35db60d67ffc940
JorgoVassiliadis <104491218+JorgoVassiliadis@users.noreply.github.com>
2022-07-06 10:39:23 +0200

    Update 8_ ❔_Fragesession.py

--- pages/8_ ❔_Fragesession.py
+++ pages/8_ ❔_Fragesession.py
@@ -6,7 +6,10 @@ st.set_page_config(
 )
 
 st.header("❔ - 08.06.22 - Fragesession")
-st.markdown("""Die Fragestunde haben wir genutzt, um abzuklären ob auch unser neuer Datensatz verwendet werden darf. Nachdem wir diesen nutzen dürfen, kam nun der Versuch unser trainiertes Model auf Streamlit zum Laufen zu bringen. 
+st.markdown("""
+Die Fragestunde haben wir genutzt, um abzuklären ob auch unser neuer Datensatz verwendet werden darf. Nachdem wir diesen nutzen dürfen, kam nun der Versuch unser trainiertes Model auf Streamlit zum Laufen zu bringen. 
+
 Wir mussten Inception V3, Encoder und Decoder zum laufen bringen und man musste jedes mal das Dictionary neu erzeugen lassen damit unser Captioning funktioniert. Dadurch sind wir schnell an die Grenzen von Streamlit gestoßen. 
+
 Nachdem wir sehr viel Arbeit reingesteckt haben, haben wir einsehen müssen, dass es vorerst nicht funktioniert, da wir einfach zu große Datenmengen verarbeiten. 
    """)
