103422ea970cae2c9eaeea968e83ae874ca5bf4d
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-07 12:55:37 +0200

    Update 4_📚_Related_Work.py

--- pages/4_📚_Related_Work.py
+++ pages/4_📚_Related_Work.py
@@ -18,5 +18,6 @@ st.markdown(
          - https://proceedings.neurips.cc/paper/2019/file/680390c55bbd9ce416d1d69a9ab4760d-Paper.pdf
          - https://link.springer.com/content/pdf/10.1007/978-3-030-89701-7_2.pdf (Frei downloadbar mittels FAU Zugang)
          - https://www.researchgate.net/publication/347959945_The_Traffic_Scene_Understanding_and_Prediction_Based_on_Image_Captioning/fulltext/5feecaaca6fdccdcb81eb998/The-Traffic-Scene-Understanding-and-Prediction-Based-on-Image-Captioning.pdf?origin=publication_detail
+         - https://arxiv.org/abs/1502.03044
          """
 )
