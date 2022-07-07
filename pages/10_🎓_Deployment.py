d7eb464551f1e847a295c7c9cf4c9fae4541fdcb
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-07 15:41:38 +0200

    Update 10_🎓_Deployment.py

--- pages/10_🎓_Deployment.py
+++ pages/10_🎓_Deployment.py
@@ -6,7 +6,19 @@ st.set_page_config(
 )
 
 st.header("22.06.22 - Deployment")
-st.markdown("""Die letzten Tage vor der Abgabe haben wir gebraucht, um die Streamlit app aufzuhübschen. Außerdem haben wir unsere Streamlitapp von Singlepage auf Multipages umgestellt. Dies ermöglicht eine einfachere Navigation und eine bessere Übersichtlichkeit.
+st.markdown("""Die letzten Tage vor der Abgabe haben wir gebraucht, um die Streamlit app aufzuhübschen. Einige der Änderungen waren z.B.:
+            - Umstellung der App von Single auf Multipage 
+            - Einbinden der selbst erzeugten Datensätze und Zugriff auf die Bilder
+            - Ein kleiner Ausschnitt aus dem MS COCO Datenset
+            - Einbinden von Ergebnissen während des Trainings
+            - Implementierung der Funktion ein Bild zu untertiteln
+            
+            Im Reiter Testing kann man ein eigenes Bild einem Modell, welches auf 200.000 Datenpunkten trainiert wurde übergeben und sich eine Bildunterschrift generieren lassen.
+            Leider funktionieren die entsprechenden Python Packages nicht sonderlich gut mit Streamlit, weswegen man alle 5-6 Bilder den Cache einmal clearen sollte
+            um einen Absturz der App vorzubeugen. 
+            Außerdem funktoniert das untertiteln der Bilder signifikant besser, wenn nicht viele Sachen auf dem Bild zu sehen sind. Im Zweifelsfall kann es zu sehr langen und 
+            ungenauen Captions kommen, da das Modell die schiere Menge an Features schlecht verarbeiten kann. Beispiele für gute Bilder sind im Reiter Feedback zu sehen.
+           
    
    
    """)
