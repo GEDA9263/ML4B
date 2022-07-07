1ca484c0c27dc8640ec39d16d527e9ddb93439cf
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-07 15:42:11 +0200

    Update 10_🎓_Deployment.py

--- pages/10_🎓_Deployment.py
+++ pages/10_🎓_Deployment.py
@@ -7,6 +7,7 @@ st.set_page_config(
 
 st.header("22.06.22 - Deployment")
 st.markdown("""Die letzten Tage vor der Abgabe haben wir gebraucht, um die Streamlit app aufzuhübschen. Einige der Änderungen waren z.B.:
+
             - Umstellung der App von Single auf Multipage 
             - Einbinden der selbst erzeugten Datensätze und Zugriff auf die Bilder
             - Ein kleiner Ausschnitt aus dem MS COCO Datenset
@@ -18,7 +19,4 @@ st.markdown("""Die letzten Tage vor der Abgabe haben wir gebraucht, um die Strea
             um einen Absturz der App vorzubeugen. 
             Außerdem funktoniert das untertiteln der Bilder signifikant besser, wenn nicht viele Sachen auf dem Bild zu sehen sind. Im Zweifelsfall kann es zu sehr langen und 
             ungenauen Captions kommen, da das Modell die schiere Menge an Features schlecht verarbeiten kann. Beispiele für gute Bilder sind im Reiter Feedback zu sehen.
-           
-   
-   
-   """)
+            """)
