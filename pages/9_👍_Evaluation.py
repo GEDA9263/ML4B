43720a5a2f5f26c15628b5bed2d9eb1f67702ce6
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-07 15:14:56 +0200

    Update 9_👍_Evaluation.py

--- pages/9_👍_Evaluation.py
+++ pages/9_👍_Evaluation.py
@@ -7,9 +7,17 @@ st.set_page_config(
 
 st.header("👍 - 15.06.22 - Evaluation")
 st.markdown("""Zur Evaluation unserer Modelle haben wir einen Loss Plot erstellt, damit wir den Loss des Modells zum Zeitpunkt des Trainings dokumentieren und auswerten können.
-
-Zwischenzeitlich hatten wir mit Bugs zu kämpfen. Zum Beispiel wurden uns immer die maximale Anzahl an Wörtern, anstatt einer sinnvollen Anzahl, in der Caption ausgegeben. So ist zum Beispiel dieser Wörtersalat entstanden:    
-""")
+            """)
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.image("Trainingsbilder/Pferde/Pferde_Loss_Plot.jpg", caption= 'Verlustgrafik der Pferde', width=360)
+with col2:
+    st.image("Trainingsbilder/Elefanten/Verlustdiagramm 70 Epochen.png", caption = 'Verlustgrafik der Elefanten', width=360)
+with col3:
+    st.image("Trainingsbilder/Human/Verlust Menschen.jpg", caption = 'Verlustgrafik der Ansammlung', width=360)
+    
+st.markdown(""" Zwischenzeitlich hatten wir mit Bugs zu kämpfen. Zum Beispiel wurden uns immer die maximale Anzahl an Wörtern, anstatt einer sinnvollen Anzahl, in der Caption ausgegeben. So ist zum Beispiel dieser Wörtersalat entstanden:    
+            """)
 
 st.image("Trainingsbilder/brokencaption.jpg")
 
