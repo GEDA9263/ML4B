bc1ee5bdadf544ed19b118ece14e7e6705f927df
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-07 15:07:37 +0200

    Update 7_🖥️_Modeling.py

--- pages/7_🖥️_Modeling.py
+++ pages/7_🖥️_Modeling.py
@@ -10,7 +10,7 @@ st.markdown("""
 In dieser Woche haben wir angefangen unser Model zu trainieren.
 Wir haben uns auf 40.000 Bilder beschränkt, das heißt es waren 200.000 Bildunterschriften. Von diesen 200.000 Datensätzen haben wir 160.000 zum Trainieren genutzt und die restlichen 40.000 zum validieren des Models.
 
-Als erstes haben wir Inception V3 genutzt um die Bilder vorzuverarbeiten. Danach haben wir die Bildunterschriften mithilfe des TextVectorization layers in Integer Sequenzen transformiert. Hierbei haben wir eine maximale Länge von 50 Wörtern pro Catpion gewählt. Außerdem sind nur die 5.000 häufigsten Wörter aller Bildunterschriften zur Erzeugung erlaubt.
+Als erstes haben wir Inception V3 genutzt um die Bilder vorzuverarbeiten. Danach haben wir die Bildunterschriften mithilfe des TextVectorization layers in Integer Sequenzen transformiert. Hierbei haben wir eine maximale Länge von 50 Wörtern pro Caption gewählt. Außerdem sind nur die 5.000 häufigsten Wörter aller Bildunterschriften zur Erzeugung erlaubt.
 
 Aus den Daten von Inception V3 wird ein Vektor generiert. Dieser durchläuft den CNN Encoder, welcher aus einem single Fully connected layer besteht und wird anschließend durch den Decoder geschickt, um das nächste Wort vorherzusagen.
   """)
