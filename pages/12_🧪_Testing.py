ac4042fea8c18e1c966c7c48cc82b74588f55633
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-08 11:09:51 +0200

    Update 12_🧪_Testing.py

--- pages/12_🧪_Testing.py
+++ pages/12_🧪_Testing.py
@@ -60,15 +60,17 @@ def make_tokenizer():
 
 tokenizer = make_tokenizer()
 
-@st.cache(hash_funcs={"keras.utils.object_identity.ObjectIdentityDictionary": lambda _: None,
-                      "builtins.weakref": lambda _: None})
+@st.cache(allow_output_mutation=True, ttl = 1800, hash_funcs={"keras.utils.object_identity.ObjectIdentityDictionary": lambda _: None,
+                                                  "builtins.weakref": lambda _: None,
+                                                  "tensorflow.python.training.tracking.base.TrackableReference": lambda _: None,  })
 def wordIndex():
     return tf.keras.layers.StringLookup(
                                         mask_token="",
                                         vocabulary=tokenizer.get_vocabulary())
 
-@st.cache(hash_funcs={"keras.utils.object_identity.ObjectIdentityDictionary": lambda _: None,
-                      "builtins.weakref": lambda _: None})
+@st.cache(allow_output_mutation=True, ttl = 1800, hash_funcs={"keras.utils.object_identity.ObjectIdentityDictionary": lambda _: None,
+                                                  "builtins.weakref": lambda _: None,
+                                                  "tensorflow.python.training.tracking.base.TrackableReference": lambda _: None,  })
 def indexWord():
     return tf.keras.layers.StringLookup(
                                         mask_token="",
