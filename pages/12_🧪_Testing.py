cc541ae804852ae83df77ab1c116354f5ef6b196
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-08 11:32:06 +0200

    Update 12_🧪_Testing.py

--- pages/12_🧪_Testing.py
+++ pages/12_🧪_Testing.py
@@ -184,7 +184,7 @@ encoder, decoder, optimizer = makeModel()
 @st.cache(allow_output_mutation=True, ttl = 1800, hash_funcs={"keras.utils.object_identity.ObjectIdentityDictionary": lambda _: None,
                                                   "builtins.weakref": lambda _: None,
                                                   "tensorflow.python.training.tracking.base.TrackableReference": lambda _: None,
-                                                   "tensorflow.python.training.tracking.base.CheckpointPosition": lambda _: None })
+                                                   })
 def makeCKPT():
     ckpt = tf.train.Checkpoint(encoder=encoder,
                            decoder=decoder,
@@ -196,7 +196,7 @@ ckpt = makeCKPT()
 @st.cache(allow_output_mutation=True, ttl = 1800, hash_funcs={"keras.utils.object_identity.ObjectIdentityDictionary": lambda _: None,
                                                   "builtins.weakref": lambda _: None,
                                                   "tensorflow.python.training.tracking.base.TrackableReference": lambda _: None,
-                                                   "tensorflow.python.training.tracking.base.CheckpointPosition": lambda _: None })
+                                                   })
 def makeManager():
     ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=2)
     return ckpt_manager
