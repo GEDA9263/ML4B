6713639e76e02edebc644fd00c47508ff812e583
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-07-08 08:58:55 +0200

    Update 7_🖥️_Modeling.py

--- pages/7_🖥️_Modeling.py
+++ pages/7_🖥️_Modeling.py
@@ -8,7 +8,7 @@ st.set_page_config(
 st.header("🖥️ - 01.06.22 - Modeling")
 st.markdown("""
 In dieser Woche haben wir angefangen unser Model zu trainieren.
-Wir haben uns auf 40.000 Bilder beschränkt, das heißt es waren 200.000 Bildunterschriften. Von diesen 200.000 Datensätzen haben wir 160.000 zum Trainieren genutzt und die restlichen 40.000 zum validieren des Models.
+Wir haben uns auf 40.000 Bilder beschränkt, das heißt es waren 200.000 Bildunterschriften. Von diesen 200.000 Datensätzen haben wir 160.000 zum trainieren genutzt und die restlichen 40.000 zum validieren des Models.
 
 Als erstes haben wir Inception V3 genutzt um die Bilder vorzuverarbeiten. Danach haben wir die Bildunterschriften mithilfe des TextVectorization layers in Integer Sequenzen transformiert. Hierbei haben wir eine maximale Länge von 50 Wörtern pro Caption gewählt. Außerdem sind nur die 5.000 häufigsten Wörter aller Bildunterschriften zur Erzeugung erlaubt.
 
